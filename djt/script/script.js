595b63f9d6c6007ca299c3dfac2119cfc6c4150c
MartinBluszcz0403 <martinivy0403@gmail.com>
2025-06-06 19:40:00 +0100

    Delete djt directory

--- djt/script/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('buyBtn').addEventListener('click', () => {
-    window.scrollTo({
-      top: document.querySelector('.cta').offsetTop,
-      behavior: 'smooth'
-    });
-  });
-  
